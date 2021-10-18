ec627ac8c959c9fe830f438662a2795994ee07db
Kiel Arneson <kiel.arneson@gmail.com>
2021-10-18 11:56:39 -0500

    Added Github OAuth

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,14 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script>
+import axios from "axios";
+export default {
+  created: function () {
+    axios.get("/github_profile").then((response) => {
+      console.log(response.data);
+    });
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <a href="https://github.com/login/oauth/authorize?client_id=799755aee3a77ffea3a6">Sign into GitHub</a>
     <!-- <div v-for="post in posts" v-bind:key="post.id">
       <h2>{{ post.title }}</h2>
       <p>{{ post.body }}</p>
@@ -35,6 +36,14 @@ export default {
       console.log(response.data);
       this.articles = response.data;
     });
+    if (this.$route.query.code) {
+      axios.get("/auth/github/callback?code=" + this.$route.query.code).then((response) => {
+        console.log("GitHub OAuth", response.data);
+        axios.defaults.headers.common["Authorization"] = "Token " + response.data.access_token;
+        localStorage.setItem("github_access_token", response.data.access_token);
+        this.$router.push("/about");
+      });
+    }
   },
   methods: {
     // createPost: function () {
