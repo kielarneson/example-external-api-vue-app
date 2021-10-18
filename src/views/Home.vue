d8cb42d4fb163716f2806471a397a33ea35b5345
Kiel Arneson <kiel.arneson@gmail.com>
2021-10-18 09:31:51 -0500

    Added get request to created function and displayed posts

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
+    <div v-for="post in posts" v-bind:key="post.id">
+      <h2>{{ post.title }}</h2>
+      <p>{{ post.body }}</p>
+    </div>
   </div>
 </template>
 
@@ -12,10 +15,15 @@ import axios from "axios";
 export default {
   data: function () {
     return {
-      message: "Welcome to Vue.js!",
+      posts: [],
     };
   },
-  created: function () {},
+  created: function () {
+    axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
+      console.log(response.data);
+      this.posts = response.data;
+    });
+  },
   methods: {},
 };
 </script>
