cc2ea6bb05dfd51e6c67feada21094c5bdd6b378
Kiel Arneson <kiel.arneson@gmail.com>
2021-10-18 10:12:18 -0500

    Added example of making web requests to rails backend to make web requests to third party api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,11 @@
       <h2>{{ post.title }}</h2>
       <p>{{ post.body }}</p>
     </div>
+
+    <div v-for="article in articles" v-bind:key="article.id">
+      <h2>{{ article.title }}</h2>
+      <p>{{ article.content }}</p>
+    </div>
   </div>
 </template>
 
@@ -16,6 +21,8 @@ export default {
   data: function () {
     return {
       posts: [],
+      articles: [],
+      // newPostParams: {},
     };
   },
   created: function () {
@@ -23,7 +30,18 @@ export default {
       console.log(response.data);
       this.posts = response.data;
     });
+    axios.get("/articles").then((response) => {
+      console.log(response.data);
+      this.articles = response.data;
+    });
   },
-  methods: {},
+  methods: {
+    // createPost: function () {
+    //   axios.post("https://jsonplaceholder.typicode.com/posts", this.newPostParams).then((response) => {
+    //     console.log(this.newPostParams);
+    //     this.posts.push(response.data);
+    //   });
+    // },
+  },
 };
 </script>
